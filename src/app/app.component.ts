cc468925cf9e69375bea1a215be03876af71ba18
Plaksa Anastasia <plaksanasta13@gmail.com>
2024-02-16 16:10:12 +0300

    End of 3 part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ɵBrowserAnimationBuilder } from "@angular/animations";
+import { CommonModule } from "@angular/common";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HttpClientModule,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'Dating app';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {                                                            //?
+    this.http.get('http://localhost:5001/api/users').subscribe({                //?
+      next: response => this.users = response,                                  //?
+      error: error => console.log(error),                                       //?
+      complete: () => console.log('Request has completed')                       //?
+    })
+  } 
 }
